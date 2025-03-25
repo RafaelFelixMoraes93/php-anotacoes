0adfcaeb42ed910b6add68143f0aa6fbb4df2704
Rafael Felix de Moraes <119057205+RafaelFelixMoraes93@users.noreply.github.com>
2025-03-25 11:12:54 -0300

    Create 001 - Servidor * Cliente.php

---
+++ 001 - Servidor * Cliente.php
@@ -0,0 +1,14 @@
+    Lado servidor
+<body>
+  <h1>Testando PHP</h1>
+  <?php
+    $nome = "Rafael";
+    echo "<p>Eu me chamo $nome!</p>";
+  ?>
+</body>
+------------------------------------------
+    Lado cliete
+<body>
+  <h1>Testando PHP</h1>  
+  <p>Eu me chamo Rafael!</p>";  
+</body>
